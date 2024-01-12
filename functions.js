825b1fa33f246d8d1812d207522e670ede66c8de
jomeis <113468648+jomeis@users.noreply.github.com>
2024-01-12 09:44:36 -0800

    Update functions.js

--- functions.js
+++ functions.js
@@ -3,8 +3,6 @@ function saveAndDisplay() {
     var numb1 = document.getElementById('numb1').value;
     var numb2 = document.getElementById('numb2').value;
     var selectedOperation = document.getElementById('operation').value;
-    var numb1Text = document.getElementById('numb1Text').innerText
-    var numb2Text = document.getElementById('numb2Text').innerText
 
     console.log('User Input 1:', numb1);
     console.log('User Input 2:', numb2);
@@ -12,23 +10,23 @@ function saveAndDisplay() {
     switch(selectedOperation) {
         case 'subtract': 
             var answer = +numb1 - +numb2; // -
-            document.getElementById('numb1Text').innerText = "Enter first numb:";
-            document.getElementById('numb2Text').innerText = "Enter second numb:";
+            document.getElementById('numb1Text').innerText = "Minuend:";
+            document.getElementById('numb2Text').innerText = "Subtrahend:";
             break;
         case 'add':
             var answer = +numb1 + +numb2; // +
-            document.getElementById('numb1Text').innerText = "Enter first numb:";
-            document.getElementById('numb2Text').innerText = "Enter second numb:";
+            document.getElementById('numb1Text').innerText = "First Addend:";
+            document.getElementById('numb2Text').innerText = "Second Addend";
             break;
         case 'divide':
             var answer = +numb1 / +numb2; // /
-            document.getElementById('numb1Text').innerText = "Enter first numb:";
-            document.getElementById('numb2Text').innerText = "Enter second numb:";
+            document.getElementById('numb1Text').innerText = "Dividend:";
+            document.getElementById('numb2Text').innerText = "Divisor:";
             break;
         case 'multiply':
             var answer = +numb1 * +numb2; // *
-            document.getElementById('numb1Text').innerText = "Enter first numb:";
-            document.getElementById('numb2Text').innerText = "Enter second numb:";
+            document.getElementById('numb1Text').innerText = "Multiplicand:";
+            document.getElementById('numb2Text').innerText = "Multiplier:";
             break;
         case 'caret':
             var answer = Math.pow(+numb1, +numb2); // ^
@@ -45,6 +43,8 @@ function saveAndDisplay() {
             document.getElementById('numb1Text').innerText = "Base:";
             document.getElementById('numb2Text').innerText = "Argument:";
             break;  
+        default:
+            answer = "what are you doing??";
       }
 
     document.getElementById('answer').innerText = answer;
